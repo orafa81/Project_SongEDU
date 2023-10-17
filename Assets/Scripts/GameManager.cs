de9ad2a4d05cce723d6d9d435fcf77cf9e5dfef8
pedro@gmail.com <pedro@gmail.com>
2023-10-17 10:28:20 -0300

    Segundo Upgrade

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,19 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public GameObject correctInstrument; // Este é o instrumento correto que o jogador deve escolher.
-    public Image outlineImage; // A imagem de contorno.
 
     public void CheckAnswer(Button buttonClicked)
     {
         if (buttonClicked.gameObject == correctInstrument)
         {
             // A resposta está correta. Exiba o contorno verde no objeto do instrumento correto.
-            outlineImage.color = Color.green;
+            
             Debug.Log("Resposta correta!");
         }
         else
         {
             // A resposta está errada. Exiba o contorno vermelho no botão clicado.
-            outlineImage.color = Color.red;
+            
             Debug.Log("Resposta incorreta.");
         }
     }
